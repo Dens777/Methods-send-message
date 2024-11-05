3394e3071b0396ac1c013a49e6c8c234e2f91342
Dens777 <dens77712@gmail.com>
2024-11-05 11:54:24 +0300

    Add files via upload

---
+++ module_13_3.py
@@ -0,0 +1,19 @@
+from aiogram import Bot,Dispatcher,executor,types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api =''
+bot =Bot(token=api)
+db =Dispatcher(bot,storage=MemoryStorage())
+
+@db.message_handler(commands='start')
+async def all_message(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+
+@db.message_handler()
+async def all_message(message):
+    await message.answer ("Введите команду '/start', чтобы начать общение.")
+
+
+if __name__=='__main__':
+    executor.start_polling(db,skip_updates=True)
